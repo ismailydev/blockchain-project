f3b9561df2b2f665ccb9fa11fc81cd1cc9c6c9f1
Ismail Yimer <ismailydev@gmail.com>
2024-06-28 22:56:01 +0300

    connect tokens api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,60 +13,19 @@ import Image from 'next/image';
 import { BNBIcon, BTCIcon, SettingsIcon, SwapIcon, TrendUpIcon } from '@/constants/icons';
 import { ChevronRightIcon } from 'lucide-react';
 import { BNBLineChart, BTCBarChart, BTCLineChart } from '@/constants/images';
+import axios from 'axios';
+import { Token } from '@prisma/client';
 
-const tokens = [
-  {
-    name: 'BTC',
-    currency: 'USD',
-    lastTrade: '63000.0',
-    percent24h: '-2.21',
-    change24h: '-1426.18',
-  },
-  {
-    name: 'ETH',
-    currency: 'USD',
-    lastTrade: '3408.9',
-    percent24h: '-0.33',
-    change24h: '-11.2',
-  },
-  {
-    name: 'DOGE',
-    currency: 'USD',
-    lastTrade: '0.15',
-    percent24h: '15.75',
-    change24h: '0.02',
-  },
-  {
-    name: 'ALGO',
-    currency: 'USD',
-    lastTrade: '0.15',
-    percent24h: '0.0',
-    change24h: '0.0',
-  },
-  {
-    name: 'DOT',
-    currency: 'USD',
-    lastTrade: '5.64',
-    percent24h: '0.0',
-    change24h: '0.0',
-  },
-  {
-    name: 'UNI',
-    currency: 'USD',
-    lastTrade: '9.79',
-    percent24h: '0.0',
-    change24h: '0.0',
-  },
-  {
-    name: 'COMP',
-    currency: 'USD',
-    lastTrade: '45.67',
-    percent24h: '-0.95',
-    change24h: '-0.44',
-  },
-];
+const getTokens = async (): Promise<Token[]> => {
+  const res = await axios('http://localhost:3001/api/tokens');
+  console.log('res', res.data);
 
-export default function Home() {
+  return await res.data;
+};
+
+export default async function Home() {
+  const tokens = await getTokens();
+
   return (
     <main className="p-24">
       <div className="max-w-6xl mx-auto flex flex-col gap-20">
