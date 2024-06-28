3b7470310026bc4e2d84632c2023dbafe81dace2
Ismail Yimer <ismailydev@gmail.com>
2024-06-28 08:58:11 +0300

    landing page and header section

---
+++ src/constants/icons.ts
@@ -0,0 +1,7 @@
+import BNBIcon from '@/assets/icons/bnb.svg';
+import BTCIcon from '@/assets/icons/btc.svg';
+import SettingsIcon from '@/assets/icons/settings-icon.svg';
+import SwapIcon from '@/assets/icons/swap.svg';
+import TrendUpIcon from '@/assets/icons/trend-up.svg';
+
+export { BNBIcon, BTCIcon, SettingsIcon, SwapIcon, TrendUpIcon };
---
+++ src/constants/images.ts
@@ -0,0 +1,8 @@
+import Logo from '@/assets/images/logo.png';
+import Background from '@/assets/images/background.png';
+
+import BTCLineChart from '@/assets/images/btc-line-chart.png';
+import BNBLineChart from '@/assets/images/bnb-line-chart.png';
+import BTCBarChart from '@/assets/images/btc-bar-chart.png';
+
+export { Logo, Background, BTCLineChart, BNBLineChart, BTCBarChart };
